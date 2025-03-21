e864bd92e4388bcf0e4f58089e0a96d8633a32c4
Timon Schelling <me@timon.zip>
2025-03-21 03:11:11 +0000

    feat: implement simple current word selection slider

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,10 +1,10 @@
-use leptos::prelude::*;
+use leptos::{logging, prelude::*};
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_mview::mview;
 use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment,
 };
-use leptos_mview::mview;
 use leptos_use::{use_interval, UseIntervalReturn};
 use lipsum::lipsum;
 
@@ -39,9 +39,7 @@ pub fn App() -> impl IntoView {
 
     mview! {
         Stylesheet href="/pkg/app.css";
-
-        Title text="Welcome to Leptos";
-
+        Title text="Literacy";
         Router {
             main {
                 Routes fallback={|| "Page not found.".into_view()} {
@@ -54,27 +52,122 @@ pub fn App() -> impl IntoView {
 
 #[component]
 fn ReadingPage() -> impl IntoView {
-    let UseIntervalReturn { counter, .. } = use_interval(550);
+    mview! {
+        Reader;
+    }
+}
+
+#[component]
+fn Reader() -> impl IntoView {
+    let text_offset = RwSignal::new(0u32);
+
+    let text_length = 100u32;
+
+    let text_gen = move |length: u32| {
+        lipsum(length.try_into().unwrap_or(text_length as usize))
+    };
+    let text = text_gen(text_length);
+    let text = text
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let (content, set_content) = signal(text);
+
+    let UseIntervalReturn { counter: new_text, .. } = use_interval(3000);
+    Effect::new(move || {
+        new_text.get();
+        let offset = text_offset.get_untracked();
+        let new = text_gen(text_length + offset)
+            .split_whitespace()
+            .skip(offset as usize)
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+        logging::log!("{}", new.join(" "));
+        set_content.set(new);
+        text_offset.set(offset + 1);
+    });
+
+
+    let content_length = RwSignal::new(0u32);
+
+    let progress_write = RwSignal::new(Some(0u32));
+    let progress = progress_write.read_only();
+
+    let UseIntervalReturn { counter, reset: counter_reset ,.. } = use_interval(550);
+    Effect::new(move || {
+        counter.get();
+        if progress.get_untracked().unwrap_or(0) > content_length.get() {
+            return;
+        }
+        progress_write.set(Some(progress.get_untracked().unwrap_or(0) + 1));
+    });
+
+    let content_clone = content.clone();
+    Effect::new(move || {
+        content_length.set(content_clone.get().len().try_into().unwrap_or(0));
+    });
+    let content_clone = content.clone();
+    Effect::new(move || {
+        content_clone.get();
+        progress_write.set(None);
+        counter_reset();
+    });
 
     mview! {
         div.reader {
-            div.page {
-                Paragraph text={lipsum(300)} highlight={Some(counter)};
+            Controls progress={progress_write} content_length={content_length.read_only()};
+            div.pages {
+                Page content={content} highlight={progress};
             }
         }
     }
 }
 
 #[component]
-fn Paragraph(text: String, highlight: Option<Signal<u64>>) -> impl IntoView {
+fn Controls(progress: RwSignal<Option<u32>>, content_length: ReadSignal<u32>) -> impl IntoView {
+    let internal = RwSignal::new("".to_string());
+    Effect::new(move || internal.set(progress.get().unwrap_or(0).to_string()));
+    Effect::new(move || {
+        let new = internal.get().parse().unwrap_or(0);
+        if new == progress.get().unwrap_or(0) {
+            return;
+        }
+        progress.set(Some(new))
+    });
+
+    mview! {
+        div.controls {
+            div.progress {
+                input
+                    type="range"
+                    min="0"
+                    max={content_length}
+                    bind:value={internal};
+            }
+        }
+    }
+}
+
+#[component]
+fn Page(content: ReadSignal<Vec<String>>, highlight: ReadSignal<Option<u32>>) -> impl IntoView {
+    mview! {
+        div.page {
+            Paragraph text={content()} {highlight};
+        }
+    }
+}
+
+#[component]
+fn Paragraph(text: Vec<String>, highlight: ReadSignal<Option<u32>>) -> impl IntoView {
     mview! {
         p.paragraph {
             {
                 move || text
-                    .split_whitespace()
+                    .iter()
                     .enumerate()
                     .map(|(i, w)| {
-                        let is_highlighted = highlight.as_ref().map_or(false, move |h| h.get() == i as u64);
+                        let is_highlighted = highlight.get().map_or(false, move |h| h == i as u32);
                         mview! {
                             Word
                                 text={w.to_string()}
